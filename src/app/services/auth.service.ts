a1d3a53d1b91f5c8a7b11bba46d6a86039b456bf
Umut Bilal Okur <umutokur11@outlook.com>
2023-03-27 02:37:29 +0300

    login register auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,6 +6,10 @@ import {
 } from '@angular/fire/compat/firestore';
 import Instructor from '../models/instructor.model';
 import Student from '../models/student.model';
+import { Observable, of } from 'rxjs';
+import { delay, map, filter, switchMap } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -13,13 +17,27 @@ import Student from '../models/student.model';
 export class AuthService {
   private instructorCollection: AngularFirestoreCollection<Instructor>;
   private studentCollection: AngularFirestoreCollection<Student>;
+  public isAuthenticated$: Observable<boolean>;
+  public isAuthenticatedWithDelay$: Observable<boolean>;
+  public redirect = false;
 
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFirestore,
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     this.instructorCollection = this.db.collection('instructors');
     this.studentCollection = this.db.collection('students');
+    this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map(e => this.route.firstChild),
+      switchMap(route => route?.data ?? of({})),
+    ).subscribe(data => {
+      this.redirect = data['authOnly'] ?? false;
+    })
   }
 
   public async createInstructor(instructorData: Instructor) {
@@ -70,4 +88,15 @@ export class AuthService {
     });
   }
 
+  public async logout($event?: Event) {
+    if ($event) {
+      $event.preventDefault();
+    }
+
+    await this.auth.signOut();
+
+    if (this.redirect) {
+      await this.router.navigate(['/']);
+    }
+  }
 }
